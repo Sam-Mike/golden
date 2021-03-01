1d13ae3b789a35cb16043a010affdaeb03b40ec3
unknown <samikemashiku@gmial.com>
2021-03-01 09:40:30 +0300

    erctifying corrup .git folder

--- resources/js/router.js
+++ resources/js/router.js
@@ -72,22 +72,22 @@ const router = new VueRouter({
 
 const authStatus = store.getters["auth/authenticated"]
 
-router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    // this route requires auth, check if logged in
-    // if not, redirect to login page.
-    if (!authStatus.authenticated) {
-      next({
-        path: '/login',
-        query: { redirect: to.fullPath }
-      })
-    } else {
-      next()
-    }
-  } else {
-    next() // make sure to always call next()!
-  }
-})
+// router.beforeEach((to, from, next) => {
+//   if (to.matched.some(record => record.meta.requiresAuth)) {
+//     // this route requires auth, check if logged in
+//     // if not, redirect to login page.
+//     if (!authStatus.authenticated) {
+//       next({
+//         path: '/login',
+//         query: { redirect: to.fullPath }
+//       })
+//     } else {
+//       next()
+//     }
+//   } else {
+//     next() // make sure to always call next()!
+//   }
+// })
 
 export default router;
 
